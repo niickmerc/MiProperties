db3fce8a57c2047d783d225be5206941b1fcbaa4
niickmerc <nickdmercuri@gmail.com>
2022-02-23 13:01:08 -0800

    Checking code coverage score - Phase #2 is implemented

--- src/test/model/PropertyTest.java
+++ src/test/model/PropertyTest.java
@@ -1,15 +1,29 @@
 package model;
 
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PropertyTest {
 
     Property p1;
+    Property p2;
+    Property p3;
+    ArrayList<Tenant> testTenants = new ArrayList<>();
 
     @BeforeEach
     public void setup(){
+
         p1 = new Property("2133 University Avenue", 1250000, 3600);
+        p2 = new Property("1234 Testing St", 1000000, 2500, new ArrayList<Tenant>());
+
+        testTenants.add(new Tenant("John Doe"));
+
+        p3 = new Property("415 Caledonia Ave", 2300000, 4000, testTenants);
+
+
     }
 
     @Test
@@ -19,6 +33,25 @@ public class PropertyTest {
         assertEquals(3600, p1.getMonthlyRent());
         assertFalse(p1.getIsRented());
         assertTrue(p1.getTenantList().isEmpty());
+    }
+
+    @Test
+    public void testConstructorAltConstructorEmptyTenantList() {
+        assertEquals("1234 Testing St", p2.getCivicAddress());
+        assertEquals(1000000, p2.getPropertyValue());
+        assertEquals(2500, p2.getMonthlyRent());
+        assertTrue(p2.getTenantList().isEmpty());
+        assertFalse(p2.getIsRented());
+    }
+
+    @Test
+    public void testConstructorAltConstructorWithSomeTenants() {
+        assertEquals("415 Caledonia Ave", p3.getCivicAddress());
+        assertEquals(2300000, p3.getPropertyValue());
+        assertEquals(4000, p3.getMonthlyRent());
+        assertFalse(p3.getTenantList().isEmpty());
+        assertEquals(1, p3.getTenantList().size());
+        assertTrue(p3.getIsRented());
     }
 
     @Test
