bf37c6612cc94240310e8f6449aad11b6c1ea8ae
niickmerc <nickdmercuri@gmail.com>
2022-03-15 22:51:44 -0700

    Continuing GUI development

--- src/main/ui/MainV2.java
+++ src/main/ui/MainV2.java
@@ -2,6 +2,8 @@ package ui;
 
 public class MainV2 {
     public static void main(String[] args) {
+
+        //LaunchPage launchPage = new LaunchPage();
         new PropertyManagementAppGUI();
     }
 }
--- src/main/ui/PropertyManagementAppGUI.java
+++ src/main/ui/PropertyManagementAppGUI.java
@@ -4,14 +4,19 @@ import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PropertyManagementAppGUI extends JFrame {
+public class PropertyManagementAppGUI extends JFrame implements ActionListener {
 
     // Setting JFrame width and height
     private static final int WIDTH = 1200;
     private static final int HEIGHT = 600;
+    private static final Color BACKGROUND_COLOR = new Color(45,47, 48);
 
     // Swing components needed
+    private JFrame frame;
     private JMenuBar menuBar;
     private JButton addCommand;
     private JButton deleteCommand;
@@ -28,37 +33,74 @@ public class PropertyManagementAppGUI extends JFrame {
         jsonReader = new JsonReader(JSON_STORE);
         jsonWriter = new JsonWriter(JSON_STORE);
 
-        initializeGraphics();
-      //  initializeActionListeners();
-    }
+        // Create JFrame
+        frame = new JFrame("MiProperties  V2.0.1");
 
-    public void initializeGraphics() {
-
-        JFrame frame = new JFrame("Propulus  V2.0.1");
+        // Customize JFrame Attributes
         frame.setSize(WIDTH, HEIGHT);
-        frame.setLocation(100, 100);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int widthDimension = dimension.width / 2 - frame.getSize().width / 2;
+        int heightDimension = dimension.height / 2 - frame.getSize().height / 2;
+        frame.setLocation(widthDimension, heightDimension);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
 
-        initializeMenuBar();
+        // Creating Main Panel (Displays list of currently-owned properties)
+        JPanel propertiesPanel = new JPanel();
+        propertiesPanel.setPreferredSize(new Dimension(800, HEIGHT));
+        propertiesPanel.setBackground(BACKGROUND_COLOR);
+
+        JLabel propertiesLabel = new JLabel("Properties Panel Placeholder.");
+        propertiesLabel.setForeground(Color.WHITE);
+        propertiesLabel.setFont(new Font("Avenir", 10, 20));
+        propertiesPanel.add(propertiesLabel);
+        frame.add(propertiesPanel, BorderLayout.WEST);
+
+        // Create Summary Panel (Will display portfolio summary statistics in real-time)
+        JPanel summaryPanel = new JPanel();
+        summaryPanel.setPreferredSize(new Dimension(400, HEIGHT));
+        summaryPanel.setBackground(Color.GRAY);
+        JLabel summaryLabel = new JLabel("Summary Panel Placeholder.");
+        summaryLabel.setForeground(Color.WHITE);
+        summaryLabel.setFont(new Font("Avenir", 10, 20));
+        summaryPanel.add(summaryLabel);
+        frame.add(summaryPanel, BorderLayout.EAST);
+
+        // Create Option Panel (Will display all behaviour available to users)
+        // EVENTUALLY: buildOptionPanel();
+        JPanel optionPanel = new JPanel();
+        optionPanel.setPreferredSize(new Dimension(WIDTH, 50));
+        optionPanel.setBackground(Color.BLACK);
+        optionPanel.setLayout(new FlowLayout());
+        frame.add(optionPanel, BorderLayout.SOUTH);
+
+        // Create Menu Bar
+        buildMenuBar();
+
 
         frame.setVisible(true);
     }
 
-    public void initializeMenuBar() {
-
-        menuBar = new JMenuBar();
-        JMenu save = new JMenu("Save");
-        JMenuItem savePortfolio = new JMenuItem("Save Portfolio");
-        save.add(savePortfolio);
-        menuBar.add(save);
-        JMenu load = new JMenu("Load");
-        JMenuItem loadPortfolio = new JMenuItem("Load Portfolio");
-        load.add(loadPortfolio);
-        menuBar.add(load);
-
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 
+    public void buildMenuBar() {
+        menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+
+        JMenuItem save = new JMenuItem("Save");
+        JMenuItem load = new JMenuItem("Load");
+        JMenuItem exit = new JMenuItem("Exit");
 
 
+        fileMenu.add(save);
+        fileMenu.add(load);
+        fileMenu.add(exit);
+
+        frame.setJMenuBar(menuBar);
+    }
 }
