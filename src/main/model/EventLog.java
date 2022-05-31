4d20502ab1bb8e713a842001def4186c8851a598
niickmerc <nickdmercuri@gmail.com>
2022-05-31 14:22:20 -0700

    A few small changes (documentation mainly)

--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -4,29 +4,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-/**
- * Represents a log of alarm system events.
- * We use the Singleton Design Pattern to ensure that there is only
- * one EventLog in the system and that the system has global access
- * to the single instance of the EventLog.
- */
 public class EventLog implements Iterable<Event> {
-    /** the only EventLog in the system (Singleton Design Pattern) */
     private static EventLog theLog;
     private Collection<Event> events;
 
-    /**
-     * Prevent external construction.
-     * (Singleton Design Pattern).
-     */
     private EventLog() {
         events = new ArrayList<Event>();
     }
 
     /**
      * Gets instance of EventLog - creates it
-     * if it doesn't already exist.
-     * (Singleton Design Pattern)
+     * if it doesn't already exist
      * @return  instance of EventLog
      */
     public static EventLog getInstance() {
